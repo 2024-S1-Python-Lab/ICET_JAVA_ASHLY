d03866a8c1f0880b0ef33170d99d768d04160ff2
ashlythomas590 <ashlythomas776@gmail.com>
2025-04-02 15:05:32 +0530

    Add files via upload

---
+++ Circle.java
@@ -0,0 +1,17 @@
+package figures; 
+public class Circle  
+{ 
+    private double radius; 
+    public Circle(double radius)  
+    {  
+        this.radius = radius; 
+ 
+ 
+    } 
+    public double calcArea()  
+    {  
+      double ar= Math.PI * radius * radius; 
+      return ar; 
+    } 
+} 
+                  
